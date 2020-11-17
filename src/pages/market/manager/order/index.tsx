0a2c71ad938cfcddaf2e977ed287ce457406e1ab
DavidJiah <51731944+DavidJiah@users.noreply.github.com>
2020-11-17 19:49:40 +0800

    add page

---
+++ src/pages/market/manager/order/index.tsx
@@ -0,0 +1,32 @@
+/*
+ * @Author: Dad
+ * @Date: 2020-11-17 15:16:35
+ * @LastEditors: Dad
+ * @LastEditTime: 2020-11-17 16:01:12
+ */
+import React from 'react';
+
+export default () => (
+  <div
+    style={{
+      height: '100%',
+      background: 'rgb(246, 245, 250)',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column',
+    }}
+  >
+    <div
+      style={{
+        fontSize: '18px',
+        color: '#313C8B',
+        fontWeight: 'bold',
+        position: 'relative',
+        top: '-50px',
+      }}
+    >
+      欢迎进入星权益业务运营系统！
+    </div>
+  </div>
+);
